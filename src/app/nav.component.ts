b5d73ec413fc06c77ef2997185e71cf441205d57
raufguy <raufguy@gmail.com>
2017-12-01 00:30:48 -0500

    JWT Local Storage logging functionality

--- src/app/nav.component.ts
+++ src/app/nav.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core'
-
+import { AuthService } from './auth.service'
 
 @Component({
     selector: 'nav',
@@ -8,12 +8,16 @@ import { Component } from '@angular/core'
             <mat-toolbar color="primary">
                 <button mat-button routerLink="/">Message Board</button>
                 <button mat-button routerLink="/messages">Messages</button>
-                <button mat-button routerLink="/register">Register</button>
+                <span style="flex: 1 1 auto"></span>
+                <button *ngIf="!auth.isAuthenticated" mat-button routerLink="/register">Register</button>
+                <button *ngIf="auth.isAuthenticated" mat-button routerLink="/">Welcome {{auth.name}}</button>
+                <button *ngIf="auth.isAuthenticated" mat-button (click)="auth.logout()">Logout</button>
+                
             </mat-toolbar>
             `
 })
 export class NavComponent{
-    constructor(){}
+    constructor(private auth : AuthService){}
 
     // messages = [{text:'some text',owner:'Tim'},{text:'other message',owner:'Bob'}];
 }
